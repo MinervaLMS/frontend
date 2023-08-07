125f3b7a1c165131a033456763e336439d9a2d78
Andrés Aparicio <andres.apa.23@gmail.com>
2023-08-07 15:40:20 -0500

    Implementación de Tailwind

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Minerva LMS',
+  description: 'Plataforma para el aprendizaje online',
+  keywords: 'lms, aprendizaje'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>Minerva Home Page</div>
+  )
+}
+
+export default Index
