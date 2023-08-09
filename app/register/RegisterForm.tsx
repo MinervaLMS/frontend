898d7090010bfbe0cdde6e6bca0fc63df511da31
Andrés Aparicio <andres.apa.23@gmail.com>
2023-08-08 20:01:32 -0500

    Styles and implementing events
    Events for inputs fields for saving the user information.
    Styling the page whit Typography, Fonts, colors and images.

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -1,22 +1,60 @@
 "use client";
-
-import * as React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
 function RegisterForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pasword, setPasword] = useState("");
+  const [email, setEmail] = useState("");
+  const [termsAndConditions, setTermsAndConditions] = useState(false);
+
+  const handleFirstNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasword(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleTermsAndConditionsChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setTermsAndConditions(event.target.checked);
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      password: pasword,
+      email: email,
+    };
+    if (termsAndConditions) {
+      console.log({
+        firstName: data.get("firstName"),
+        lastName: data.get("lastName"),
+        password: data.get("password"),
+        email: data.get("email"),
+      });
+    }
   };
 
   return (
@@ -25,6 +63,7 @@ function RegisterForm() {
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <TextField
+              onChange={handleFirstNameChange}
               autoComplete="given-name"
               name="firstName"
               required
@@ -32,30 +71,36 @@ function RegisterForm() {
               id="firstName"
               label="Nombres"
               autoFocus
+              size="small"
             />
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
+              onChange={handleLastNameChange}
               required
               fullWidth
               id="lastName"
               label="Apellidos"
               name="lastName"
               autoComplete="family-name"
+              size="small"
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
+              onChange={handleEmailChange}
               required
               fullWidth
               id="email"
               label="Correo"
               name="email"
               autoComplete="email"
+              size="small"
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
+              onChange={handlePasswordChange}
               required
               fullWidth
               name="password"
@@ -63,16 +108,30 @@ function RegisterForm() {
               type="password"
               id="password"
               autoComplete="new-password"
+              size="small"
             />
           </Grid>
           <Grid item xs={12}>
             <FormControlLabel
-              control={<Checkbox value="termsAndConditions" color="primary" />}
+              control={
+                <Checkbox
+                  value="termsAndConditions"
+                  color="primary"
+                  onChange={handleTermsAndConditionsChange}
+                />
+              }
               label={
-                <span>
-                  He leído y acepto los <a href="">Términos de uso</a> y la{" "}
-                  <a href="">Aviso de privacidad</a> de Minerva.*
-                </span>
+                <p>
+                  He leído y acepto los{" "}
+                  <a href="#" target="_blank">
+                    Términos de uso
+                  </a>{" "}
+                  y la{" "}
+                  <a href="#" target="_blank">
+                    Aviso de privacidad
+                  </a>{" "}
+                  de Minerva.*
+                </p>
               }
             />
           </Grid>
@@ -82,17 +141,10 @@ function RegisterForm() {
           fullWidth
           variant="contained"
           color="success"
-          sx={{ mt: 3, mb: 2 }}
+          sx={{ mt: 1, mb: 2 }}
         >
           Registrarse
         </Button>
-        <Grid container justifyContent="flex-end">
-          <Grid item>
-            <Link href="#" variant="body2">
-              Ya estas registrado? Inicia sesión
-            </Link>
-          </Grid>
-        </Grid>
       </Box>
     </div>
   );
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,67 +1,67 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import RegisterForm from "./RegisterForm";
-import styles from "./register.module.css";
-
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import styles from "@/styles/Register.module.css";
+import bg from "@/public/assets/images/register-bg.png";
+import Image from "next/image";
 
 export default function SignUp() {
   return (
-    <main className={styles.registerContainer}>
+    <main
+      className={styles.registerContainer}
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
       <CssBaseline />
       <Box
         sx={{
-          paddingTop: 8,
+          paddingTop: 2,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className={styles.vercelLogo}
+          width={100}
+          height={100}
+          priority
+        />
         <Box
-            className={styles.registerBox}
-          maxWidth="50%"
+          className={styles.registerBox}
+          maxWidth="45%"
           sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
+          marginTop={"1rem"}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="inherit">
             Regístrate
           </Typography>
           <Typography component="p" variant="inherit" align="center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-            exercitationem temporibus assumenda quos, dolore nemo?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />
+            Architecto exercitationem temporibus assumenda quos, dolore nemo?
           </Typography>
           <RegisterForm></RegisterForm>
         </Box>
+        <Typography
+          component="p"
+          variant="inherit"
+          color="#fff"
+          marginTop={"2rem"}
+        >
+          Si tienes alguna dificultad comunicate con nuestro{" "}
+          <Link href="#" style={{ color: "#fff", textDecorationColor: "#fff" }}>
+            Centro de atención.
+          </Link>
+        </Typography>
       </Box>
-      <Copyright sx={{ mt: 5 }} />
     </main>
   );
 }
