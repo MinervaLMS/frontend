8709c516f21a54044ea246719d27fa8f0e7fca90
David Mateo García Vallejo <davgarciava@unal.edu.co>
2023-08-17 00:43:35 -0500

    Validation in register form fields and loader implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
 
-import CustomSnackbar from '@/components/global/CustomSnackbar'
-import { Button } from '@mui/material'
+import CustomSnackbar from '@/components/global/CustomSnackbar';
+import Button from '@mui/material/Button';
 import * as React from 'react';
 
 export default function Home() {
--- config/interfaces.ts
+++ config/interfaces.ts
@@ -1,6 +1,6 @@
 export interface API_RegisterRequest {
-    firstName: string;
-    lastName: string;
+    first_name: string;
+    last_name: string;
     email: string;
     password: string;
 }
