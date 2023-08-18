e06ba5ecb33fb1a605a0ea98d65580a0cafbc2e9
David Mateo García Vallejo <davgarciava@unal.edu.co>
2023-08-18 18:14:48 -0500

    Course navigation implementation

---
+++ app/course/[alias]/page.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import React, { useState } from "react";
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { DRAWER_WIDTH } from "@/config/constants";
+import CourseAppBar from "@/components/features/CourseAppBar";
+import CourseDrawerList from "@/components/features/CourseDrawerList";
+
+// This functional component is the index page for the /course rute.
+// It contains the CourseAppBar and CourseDrawerList components.
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+	flexGrow: 1,
+	transition: theme.transitions.create('margin', {
+		easing: theme.transitions.easing.sharp,
+		duration: theme.transitions.duration.leavingScreen,
+	}),
+  marginLeft: `-${DRAWER_WIDTH}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+	padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function Course({
+  params,
+}: {
+  params: { alias: string };
+}) {
+	const theme = useTheme();
+	const [open, setOpen] = useState(true);
+
+	const handleDrawerOpen = (event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		setOpen(true);
+	};
+
+	const handleDrawerClose = (event: React.MouseEvent<HTMLButtonElement>
+	) => {
+		setOpen(false);
+	};
+
+	// Render the principal container for the course page.
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<CourseAppBar open={open} handleDrawerOpen={handleDrawerOpen}/>
+			<Drawer
+				sx={{
+				width: DRAWER_WIDTH,
+				flexShrink: 0,
+				'& .MuiDrawer-paper': {
+						width: DRAWER_WIDTH,
+						boxSizing: 'border-box',
+				},
+				}}
+				variant="persistent"
+				anchor="left"
+				open={open}
+			>
+				<DrawerHeader>
+					<Typography variant="h6" noWrap component="div">
+						Curso {params.alias}
+					</Typography>
+					<IconButton onClick={handleDrawerClose}>
+						{theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+					</IconButton>
+				</DrawerHeader>
+				<CourseDrawerList params={params}/>
+			</Drawer>
+			<Main open={open} sx={{ padding: {xs: theme.spacing(10, 3), sm: theme.spacing(3)} }}>
+				<DrawerHeader />
+				<Typography paragraph>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+				enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+				imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+				Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+				Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+				adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+				nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+				leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+				feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+				consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+				sapien faucibus et molestie ac.
+				</Typography>
+				<Typography paragraph>
+				Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+				eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+				neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+				tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+				sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+				tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+				gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+				et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+				tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+				eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+				posuere sollicitudin aliquam ultrices sagittis orci a.
+				</Typography>
+			</Main>
+		</Box>
+	);
+}
--- config/constants.ts
+++ config/constants.ts
@@ -2,3 +2,5 @@
 export const PASSWORD_MIN_LENGTH: number = 8;
 
 export const AUTOHIDE_ALERT_DURATION: number = 3000;
+
+export const DRAWER_WIDTH: number = 200;
