33a8ebce697ae985619eb7e749fba0e2e765a54d
Andrés Aparicio <andres.apa.23@gmail.com>
2023-08-18 18:23:16 -0500

    Contact main structure

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,71 @@
-import MainHeader from '@/components/global/MainHeader'
-import React from 'react'
+"use client";
+
+import MainHeader from "@/components/global/MainHeader";
+import React, { use } from "react";
+import styles from "@/styles/Contact.module.css";
+import { Box, IconButton, Typography } from "@mui/material";
+import { Facebook, Instagram } from "@mui/icons-material";
 
 function page() {
   return (
     <>
-      <MainHeader />
+      <main>
+        <MainHeader />
+        <Box component="section" className={styles.mainContainer}>
+          <Box
+            component="div"
+            className={`${styles.contactSection} ${styles.contactFormSection}`}
+          >
+            <Typography component="h1">Contáctanos</Typography>
+          </Box>
+          <Box
+            component="div"
+            className={`${styles.contactSection} ${styles.contactInfoSection}`}
+          >
+            <Typography component="h1">Siguenos</Typography>
+            <Box component='div'>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                href="#"
+                target="_blank"
+              >
+                <Facebook />
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                href="#"
+                target="_blank"
+              >
+                <Instagram />
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                href="#"
+                target="_blank"
+              >
+                <Instagram />
+              </IconButton>
+            </Box>
+            <Box component='div'>
+              <Typography component="h1">Preguntas frecuentes</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </main>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
