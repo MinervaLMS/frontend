b5db47f6d9323adf3909194ad0dc30548287ac01
Andrés Aparicio <andres.apa.23@gmail.com>
2023-08-23 20:22:55 -0500

    bug fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
-"use client";
 import Providers from "@/redux/provider";
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
-import { store } from "@/redux/store";
 
-const persistor = persistStore(store);
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -24,9 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
-        <PersistGate persistor={persistor}>
-          <Providers> {children} </Providers>
-        </PersistGate>
+        <Providers > {children} </Providers>
       </body>
     </html>
   );
