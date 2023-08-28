182569b82dfcd284ecb7f672bc97b6691bf59912
sebasnop <sevalenciazapata@gmail.com>
2023-08-28 03:10:12 -0500

    feature(theme): Proving different layout elements and statements

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,44 @@
 "use client";
 
-import Image from "next/image";
-//import styles from "@/styles/Home.module.css";
-import "@/styles/globals.css";
+import bg from "@/public/assets/images/register-bg.png";
 
-import CustomSnackbar from "@/components/common/CustomSnackbar";
-import Button from "@mui/material/Button";
 import * as React from "react";
 import { useAppSelector } from "@/redux/hook";
+import { Box, Container, Paper, Typography } from "@mui/material";
+import Image from "next/image";
 
-// Theming
-import lightTheme from "@/styles/themes/LightTheme"
-import { Box, ThemeProvider, Typography } from "@mui/material";
-import CssBaseline from '@mui/material/CssBaseline';
+import styles from "@/styles/RegisterLogin.module.css";
+import Link from "@mui/material/Link";
+import RegisterForm from "@/components/features/RegisterForm";
 
 export default function Home() {
 
   const userLoginState = useAppSelector((state) => state.persistedReducer.userLoginState.email);
 
-  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
-
-  const handleSnackbarClick = () => {
-    setSnackbarOpen(true);
-  };
-
-  const handleSnackbarClose = () => {
-    setSnackbarOpen(false);
-  };
-
   return (
 
-    <ThemeProvider theme={lightTheme}>
-      
-      <CssBaseline/>
-  
-      <main>
-        
-        <Typography variant="h1">Título h1</Typography>
-        <Typography variant="body1" gutterBottom>
-          www.html.com Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </Typography>
-        <Typography variant="h2">Título H2</Typography>
-        <Typography variant="h3">Título H3</Typography>
-        <Typography variant="h4">Título H4</Typography>
-        <Typography variant="h5" gutterBottom>Título H5</Typography>
-        <Typography variant="h6">Título H6</Typography>
-        <Typography variant="body1" gutterBottom>
-          www.html.com Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </Typography>
+    <Container
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      height: "100vh",
+      alignItems: "center"
+    }}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          //flex: 1,
+          padding: '40px',
+          margin: "30px",
+          overflow: 'auto',
+          maxWidth: "sm"
+        }}
+      >
+        <RegisterForm />
 
-        {/* Sección de pruebas */}
-        <p>{userLoginState}</p>
+      </Paper>
+  </Container>
 
-        <section>
-          {/* Botón para abrir el Snackbar */}
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSnackbarClick}
-          >
-            Mostrar Snackbar
-          </Button>
-
-          {/* Snackbar */}
-          <CustomSnackbar
-            message="Este es un mensaje de ejemplo"
-            severity="warning"
-            vertical="top"
-            horizontal="center"
-            autoHideDuration={3000}
-            open={snackbarOpen}
-            onClose={handleSnackbarClose}
-          />
-        </section>
-      </main>
-
-    </ThemeProvider>
   );
 }
