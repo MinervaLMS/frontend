cb5bebfa671aef1738e9a067b4449f12e59e2a60
Andrés Aparicio <andres.apa.23@gmail.com>
2023-09-24 22:44:17 -0500

    Fixing navigation
    Items changed on drawer miscellaneous
    User menu fixed with routes and new item

--- components/layout/CourseAppBar.tsx
+++ components/layout/CourseAppBar.tsx
@@ -1,112 +1,115 @@
-"use client";
+'use client'
 
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 // Import MaterialUI Components
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
 
 // Import styles
-import { styled } from "@mui/material/styles";
-import styles from "@/styles/Course.module.css";
+import { styled } from '@mui/material/styles'
+import styles from '@/styles/Course.module.css'
 
 // Import icons
-import MenuIcon from "@mui/icons-material/Menu";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import MenuIcon from '@mui/icons-material/Menu'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 // Import images
-import Image from "next/image";
+import Image from 'next/image'
 
 // Import constants
-import { DRAWER_WIDTH, USER_SETTINGS } from "@/config/constants";
+import { DRAWER_WIDTH, USER_SETTINGS } from '@/config/constants'
 
 // Import redux and router
-import { useAppDispatch, useAppSelector } from "@/redux/hook";
-import { setOpen } from "@/redux/features/drawerSlice";
-import { useRouter } from "next/navigation";
-import { logOut } from "@/redux/features/userLoginSlice";
-import { AccountCircle } from "@mui/icons-material";
+import { useAppDispatch, useAppSelector } from '@/redux/hook'
+import { setOpen } from '@/redux/features/drawerSlice'
+import { useRouter } from 'next/navigation'
+import { logOut } from '@/redux/features/userLoginSlice'
+import { AccountCircle } from '@mui/icons-material'
 
 interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
+  open?: boolean
 }
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open'
 })<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+    duration: theme.transitions.duration.leavingScreen
   }),
   ...(open && {
     width: `calc(100% - ${DRAWER_WIDTH}px)`,
     marginLeft: `${DRAWER_WIDTH}px`,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
+}))
 
 // This interface defines the types of the props object.
 interface CourseAppBarProps {
-  userName: string;
+  userName: string
 }
 
 function CourseAppBar({ userName }: CourseAppBarProps) {
   // Router
-  const router = useRouter();
+  const router = useRouter()
 
   // Redux states
   const open = useAppSelector(
     (state) => state.persistedReducer.drawerState.open
-  );
-  const dispatch = useAppDispatch();
+  )
+  const dispatch = useAppDispatch()
 
-  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElUser(event.currentTarget)
+  }
 
   const handleCloseUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(null);
-  };
+    setAnchorElUser(null)
+  }
 
   const handleDrawerOpenClose = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
-    dispatch(setOpen());
-  };
+    dispatch(setOpen())
+  }
   const handleLogOut = () => {
-    dispatch(logOut());
-    router.push("/");
-  };
+    dispatch(logOut())
+    router.push('/')
+  }
+  const handleNavigate = (rute: string) => {
+    router.push(rute)
+  }
 
   return (
-    <AppBar position="fixed" open={open}>
+    <AppBar position='fixed' open={open}>
       <Toolbar className={styles.toolbar}>
         <Box className={styles.logoBox}>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
+            color='inherit'
+            aria-label='open drawer'
             onClick={handleDrawerOpenClose}
-            edge="start"
-            size="large"
+            edge='start'
+            size='large'
           >
             <MenuIcon />
           </IconButton>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             <Image
               hidden={open}
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src='/vercel.svg'
+              alt='Vercel Logo'
               width={100}
               height={50}
               priority
@@ -114,41 +117,49 @@ function CourseAppBar({ userName }: CourseAppBarProps) {
           </Box>
         </Box>
         <Box className={styles.userBox}>
-          <Typography className={styles.userName} component="h2">
+          <Typography className={styles.userName} component='h2'>
             {userName}
           </Typography>
           <AccountCircle />
-          <Tooltip title="Open settings">
+          <Tooltip title='Open settings'>
             <IconButton
               onClick={handleOpenUserMenu}
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              color="inherit"
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              color='inherit'
             >
               <KeyboardArrowDownIcon />
             </IconButton>
           </Tooltip>
           <Menu
             className={styles.userMenu}
-            id="menu-appbar"
+            id='menu-appbar'
             anchorEl={anchorElUser}
             anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
+              vertical: 'top',
+              horizontal: 'right'
             }}
             keepMounted
             transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
+              vertical: 'top',
+              horizontal: 'right'
             }}
             open={Boolean(anchorElUser)}
             onClose={handleCloseUserMenu}
           >
             {USER_SETTINGS.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">{setting}</Typography>
+              <MenuItem
+                key={`user-opt-${setting.title}`}
+                onClick={handleCloseUserMenu}
+              >
+                <Typography
+                  textAlign='center'
+                  onClick={() => handleNavigate(setting.route)}
+                >
+                  {setting.title}
+                </Typography>
               </MenuItem>
             ))}
             <MenuItem onClick={handleLogOut}>Cerrar sesión</MenuItem>
@@ -156,7 +167,7 @@ function CourseAppBar({ userName }: CourseAppBarProps) {
         </Box>
       </Toolbar>
     </AppBar>
-  );
+  )
 }
 
-export default CourseAppBar;
+export default CourseAppBar
--- components/layout/MainAppBar.tsx
+++ components/layout/MainAppBar.tsx
@@ -1,132 +1,137 @@
-"use client";
+'use client'
 
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import { AccountCircle, ArrowDropDown } from "@mui/icons-material";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import styles from "@/styles/Header.module.css";
-import Image from "next/image";
-import { Button } from "@mui/material";
-import { useAppSelector, useAppDispatch } from "@/redux/hook";
-import { logOut } from "@/redux/features/userLoginSlice";
-import { useRouter } from "next/navigation";
-import { USER_SETTINGS } from "@/config/constants";
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import { AccountCircle, ArrowDropDown } from '@mui/icons-material'
+import MenuItem from '@mui/material/MenuItem'
+import Menu from '@mui/material/Menu'
+import styles from '@/styles/Header.module.css'
+import Image from 'next/image'
+import { Button } from '@mui/material'
+import { useAppSelector, useAppDispatch } from '@/redux/hook'
+import { logOut } from '@/redux/features/userLoginSlice'
+import { useRouter } from 'next/navigation'
+import { USER_SETTINGS } from '@/config/constants'
 
 export default function MainAppBar() {
   // Router
-  const router = useRouter();
+  const router = useRouter()
 
   // Redux states
   const userLoginState = useAppSelector(
     (state) => state.persistedReducer.userLoginState.login
-  );
+  )
   const userNameState = useAppSelector(
     (state) => state.persistedReducer.userLoginState.first_name
-  );
+  )
 
   // Redux dispatch
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
   // Menu states
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
   // Event handlers
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
 
   const handleNavigate = (rute: string) => {
-    router.push(rute);
-  };
+    router.push(rute)
+  }
 
   const handleLogOut = () => {
-    dispatch(logOut());
-    handleNavigate("/");
-  };
+    dispatch(logOut())
+    handleNavigate('/')
+  }
 
   // Menu options for the log in user
   const logInUserOptions = (
     <>
-      <Typography variant="h6" component="p">
+      <Typography variant='h6' component='p'>
         {userNameState}
       </Typography>
       <AccountCircle />
       <IconButton
-        size="large"
-        aria-label="account of current user"
-        aria-controls="menu-appbar"
-        aria-haspopup="true"
+        size='large'
+        aria-label='account of current user'
+        aria-controls='menu-appbar'
+        aria-haspopup='true'
         onClick={handleMenu}
-        color="inherit"
+        color='inherit'
       >
         <ArrowDropDown />
       </IconButton>
       <Menu
-        id="menu-appbar"
+        id='menu-appbar'
         anchorEl={anchorEl}
         anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
+          vertical: 'top',
+          horizontal: 'right'
         }}
         keepMounted
         transformOrigin={{
-          vertical: "top",
-          horizontal: "right",
+          vertical: 'top',
+          horizontal: 'right'
         }}
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
         {USER_SETTINGS.map((setting) => (
-          <MenuItem key={setting} onClick={handleClose}>
-            <Typography textAlign="center">{setting}</Typography>
+          <MenuItem key={`user-opt-${setting.title}`} onClick={handleClose}>
+            <Typography
+              textAlign='center'
+              onClick={() => handleNavigate(setting.route)}
+            >
+              {setting.title}
+            </Typography>
           </MenuItem>
         ))}
         <MenuItem onClick={handleLogOut}>Cerrar sesión</MenuItem>
       </Menu>
     </>
-  );
+  )
 
   // Menu options for the log out user
   const logOutUserOptions = (
     <>
       <Button
-        variant="contained"
-        color="info"
-        sx={{ mx: 1, textTransform: "none" }}
-        onClick={() => handleNavigate("/register")}
-        type="button"
+        variant='contained'
+        color='info'
+        sx={{ mx: 1, textTransform: 'none' }}
+        onClick={() => handleNavigate('/register')}
+        type='button'
       >
         Registrarse
       </Button>
       <Button
-        color="secondary"
-        variant="contained"
-        sx={{ mx: 1, textTransform: "none" }}
-        onClick={() => handleNavigate("/login")}
-        type="button"
+        color='secondary'
+        variant='contained'
+        sx={{ mx: 1, textTransform: 'none' }}
+        onClick={() => handleNavigate('/login')}
+        type='button'
       >
         Ingresar
       </Button>
     </>
-  );
+  )
 
   return (
-    <Box id="header">
-      <AppBar position="static">
+    <Box id='header'>
+      <AppBar position='static'>
         <Toolbar className={styles.mainHeader}>
           <div className={styles.topBarArea}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src='/vercel.svg'
+              alt='Vercel Logo'
               className={styles.logo}
               width={50}
               height={50}
@@ -141,5 +146,5 @@ export default function MainAppBar() {
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  )
 }
