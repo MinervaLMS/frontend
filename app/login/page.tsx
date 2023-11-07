b28eef667b2f0583fa7f8b0df7436beace88d984
sebasnop <sevalenciazapata@gmail.com>
2023-11-06 23:27:03 -0500

    style: Setting initial correct styles for Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -29,51 +29,6 @@ export default function Login() {
 
   return (
     <Box
-      component="main"
-      style={{ backgroundImage: `url(${bg.src})`, backgroundSize: "cover" }}
-      height={"100vh"}
-    >
-      <Box component="section" className={styles.mainContainer}>
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          className={styles.logo}
-          width={100}
-          height={100}
-          priority
-        />
-        <Box className={styles.formBox}>
-          <Typography component="h1" variant="h4" align="center">
-            Bienvenido
-          </Typography>
-          <Typography component="p" align="center">
-            Ingresa tus tus datos de inicio de sesión para acceder a tu cuenta.{" "}
-            <br />
-            ¡Estamos emocionados de verte de nuevo!
-          </Typography>
-          <LoginForm />
-          <Box sx={{ mt: 1 }}>
-            <Typography component="p">
-              {"¿Aún no estás registrado? - "}
-              <Link href="/register" variant="body1" color="secondary">
-                Regístrate
-              </Link>
-            </Typography>
-          </Box>
-        </Box>
-        <Typography component="p" sx={{ color: "#fff", marginTop: "2rem" }}>
-          Si tienes alguna dificultad comunícate con nuestro{" "}
-          <Link href="/contact">Centro de atención.</Link>
-        </Typography>
-        <Divider />
-      </Box>
-    </Box>
-  );
-}
-
-{/* Hello World 
-  return (
-    <Box
       component='main'
       style={{ backgroundImage: `url(${bg.src})`, backgroundSize: 'cover' }}
       className={styles.mainContainer}
@@ -116,5 +71,3 @@ export default function Login() {
     </Box>
   )
 }
-
-*/}
