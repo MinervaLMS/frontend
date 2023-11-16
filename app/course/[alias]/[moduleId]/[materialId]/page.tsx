219d685a6f64f0336ad27ad68e7a304a72663b9d
sebasnop <sevalenciazapata@gmail.com>
2023-11-15 20:36:18 -0500

    feat: Applying styles to navigation through materials.

--- app/course/[alias]/[moduleId]/[materialId]/page.tsx
+++ app/course/[alias]/[moduleId]/[materialId]/page.tsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react'
 // Import MaterialUI Components
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import { Container, Link } from '@mui/material'
+import { Button, ButtonGroup, Container, Link, Stack, Tooltip } from '@mui/material'
 
 // Import own components
 import CircularSpinner from '@/components/common/CircularSpinner'
@@ -30,8 +30,12 @@ import { useRouter, useParams } from 'next/navigation'
 import useCourseMaterial from '@/hooks/fetching/useCourseMaterial'
 import useComments from '@/hooks/fetching/useComments'
 import { API_STATUS_CODE } from '@/config/api-connections'
-import { API_MaterialObject } from '@/config/interfaces'
+import { API_MaterialObject, API_ModuleObject } from '@/config/interfaces'
 import { MATERIAL_TYPES } from '@/config/enums'
+
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import useCourseModule from '@/hooks/fetching/useCourseModule'
 
 const views: any = {
   PDF: PdfMaterial,
@@ -52,12 +56,28 @@ function Materials() {
 
   // States related to the API Fetch
   const { alias, moduleId, materialId } = useParams()
+  
   console.log(alias, moduleId, materialId)
+
   const {
     data: materialData,
     isLoading: materialIsLoading,
     error
   } = useCourseMaterial(String(materialId), userTokens.access)
+
+
+  {/* The next lines are for managing navigation through materials */}
+  const { data: moduleData, error: moduleError, isLoading: moduleIsLoading } = useCourseModule(
+    Number(moduleId),
+    userTokens.access
+  ) as { data: API_ModuleObject | null, error: any, isLoading: boolean}
+
+  {/* This is needed for not showing the "Next Material" button when is the final material */}
+  const moduleTotalMaterials = moduleData?.module_total_materials ? moduleData?.module_total_materials + 1 : 0;
+  const moduleName = moduleData?.name ? moduleData?.name : "";
+
+  console.log(moduleData)
+
 
   const { isLoading: commentsIsLoading } = useComments(
     String(materialId),
@@ -111,46 +131,53 @@ function Materials() {
   // Render the principal container for the course page.
   return (
     <Container maxWidth="md">
-          <Typography component='h1' variant='h4' gutterBottom>
-            {materialData?.name}
-          </Typography>
+      <Typography component='h1' variant='h4' gutterBottom>
+        {materialData?.name}
+      </Typography>
           
-          <Box id="materials_navigation" sx={{ paddingY: 2 }} display={"flex"} justifyContent={"space-between"}>
-            
-            <Link
-              onClick={() => {
-                router.push(`/course/${alias}/${moduleId}`)
-              }}
-              sx={{ cursor: 'pointer' }}
-              underline='hover'
-              color={''}
-              variant='body1'
-            >
-              ← Volver
-            </Link>
+      <Box id="materials_navigation" sx={{ paddingY: 2 }} display={"flex"} justifyContent={"space-between"}>
+        
+        <Typography color="text.secondary" sx={{paddingTop: 1 }}>
 
-            <Link
-              sx={{ cursor: 'pointer' }}
-              underline='hover'
-              color={''}
-              variant='body1'
-            >
-              <Typography> &lt; Anterior </Typography>
-            </Link>
-
-            <Link
-              sx={{ cursor: 'pointer' }}
-              underline='hover'
-              color={''}
-              variant='body1'
-            >
-              <Typography> Siguiente &gt; </Typography>
-            </Link>
-
-          </Box>
+        <Link
+          onClick={() => {
+            router.push(`/course/${alias}/${moduleId}`)
+          }}
+          sx={{ cursor: 'pointer' }}
+          color='inherit'
+          variant='body1'
+        >
+          Módulo {moduleId}{moduleName != "" ? ":" : ""} {moduleName}
+        </Link>
           
-          <CurrentView materialId={materialId} />
-        <CommentSection material={materialData} />
+          &nbsp;/ Material {materialId}
+        
+        </Typography>
+        
+        {/* The next lines are for navigation through materials */}
+        {/* The managing of this is not completed */}
+        <ButtonGroup aria-label='navigation through materials'>
+          
+          <Tooltip title="Anterior material">
+            <Button aria-label='back to previous material'>
+              <ChevronLeftIcon />
+            </Button>
+          </Tooltip>
+
+          <Tooltip title="Siguiente material">
+            <Button aria-label='go to next material'>
+              <ChevronRightIcon />
+            </Button>
+          </Tooltip>
+        
+        </ButtonGroup>
+
+      </Box>
+          
+      <CurrentView materialId={materialId} />
+      
+      <CommentSection material={materialData} />
+    
     </Container>
   )
 }
